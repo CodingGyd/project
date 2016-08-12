d0181e383d115be563099643116dfa89f08a17ea
964781872@qq.com <964781872@qq.com>
2016-08-13 00:14:51 +0800

    测试第二次提交

--- appserver/RookiePalmSpaceServer/src/main/java/com/codinggyd/RookiePalmSpaceServer/App.java
+++ appserver/RookiePalmSpaceServer/src/main/java/com/codinggyd/RookiePalmSpaceServer/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldhehehehe" );
+        System.out.println( "Hello Worldhehehehe12313" );
     }
 }
