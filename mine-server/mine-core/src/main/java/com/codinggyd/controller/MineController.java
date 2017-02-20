ccbc1990eafd961510a9e8cc5d4db1f084d1bbba
guoyd <guoyd@NB-GUOYD.hundsun.com>
2017-02-20 15:59:21 +0800

    .

--- mine-server/mine-core/src/main/java/com/codinggyd/controller/MineController.java
+++ mine-server/mine-core/src/main/java/com/codinggyd/controller/MineController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
