4f4b1217509af5031fc8e9dec2ab7b4ded8a5b1d
964781872@qq.com <964781872@qq.com>
2017-09-10 20:17:53 +0800

    m

---
+++ mine-client/mine-client/src/main/java/com/codinggyd/bean/Article.java
@@ -0,0 +1,74 @@
+package com.codinggyd.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Article implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4785066758102282805L;
+	private Integer id;
+	/**
+	 * 标题
+	 */
+	public String title;
+	/**
+	 * 内容
+	 */
+	public String content;
+
+	/**
+	 * 更新时间
+	 */
+	private String updatetime;
+	/**
+	 * 被阅读次数
+	 */
+	private Integer readingcount;
+	 
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUpdatetime() {
+		return updatetime;
+	}
+
+	public void setUpdatetime(String updatetime) {
+		this.updatetime = updatetime;
+	}
+
+	public Integer getReadingcount() {
+		return readingcount;
+	}
+
+	public void setReadingcount(Integer readingcount) {
+		this.readingcount = readingcount;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	 
+
+	 
+}
--- mine-client/mine-client/src/main/java/com/codinggyd/controller/MineController.java
+++ mine-client/mine-client/src/main/java/com/codinggyd/controller/MineController.java
@@ -1,20 +1,36 @@
  
 package com.codinggyd.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.DateUtils;
 
+import com.codinggyd.bean.Article;
+import com.codinggyd.bean.LearnSite;
+import com.codinggyd.bean.UtilFunction;
 import com.codinggyd.service.ILearnSiteService;
 import com.codinggyd.service.IUtilFunctionService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+ 
  
 @Controller
 public class MineController {
 	@Autowired
 	private IUtilFunctionService utilService;
-	
+	final Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	private ILearnSiteService itinfoService;
 	
@@ -28,10 +44,53 @@ public class MineController {
 		return itinfoService.listLearnSite();
 	}
 	
-	
+ 
 	//首页
 	@RequestMapping(value={"/","/index"})
-	public String index() {
+	public String index(Map<String,Object> model) {
+		String learnsiteJson = itinfoService.listLearnSite();
+		String utilJson = utilService.getUtilFunction();
+
+		ObjectMapper objectmapper =new ObjectMapper();
+		
+		List<LearnSite> learnSites = null;
+		try {
+			learnSites = objectmapper.readValue(learnsiteJson, objectmapper.getTypeFactory().constructParametricType(List.class, LearnSite.class));
+		} catch (Exception e) {
+			logger.error("format error ,{}",e);
+		}
+		
+		List<UtilFunction> utilFunctions = null;
+		try {
+			utilFunctions = objectmapper.readValue(utilJson, objectmapper.getTypeFactory().constructParametricType(List.class, UtilFunction.class));
+		} catch (Exception e) {
+			logger.error("format error ,{}",e);
+		}
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		List<Article> list2 = new ArrayList<>(); 
+		if (!CollectionUtils.isEmpty(learnSites)) {
+			for (LearnSite site : learnSites) {
+				Article article = new Article();
+				article.setId(site.getId());
+				article.setTitle(site.getTitle());
+				article.setContent(site.getContent());
+				article.setUpdatetime(format.format(site.getUpdatetime()));
+				list2.add(article);
+			}
+		}
+		
+		if (!CollectionUtils.isEmpty(utilFunctions)) {
+			for (UtilFunction site : utilFunctions) {
+				Article article = new Article();
+				article.setId(site.getId());
+				article.setTitle(site.getTitle());
+				article.setContent(site.getContent());
+				article.setUpdatetime(format.format(site.getUpdatetime()));
+				list2.add(article);
+			}
+		}
+		model.put("articleList",list2);  
 		return "index";
 	}
 	 
